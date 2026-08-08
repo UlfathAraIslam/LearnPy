6bc05a8bab8eee441d9fddee20b73658bbebe538
UlfathAraIslam <mouliajsahid@gmail.com>
2026-08-09 03:53:17 +0900

    multiple objects use same class

--- oop/practice_oop.py
+++ oop/practice_oop.py
@@ -1,26 +1,41 @@
 class Microwave:
-    def __init__(self,brand: str,power_rating: str) -> None:
+    def __init__(self, brand: str, power_rating: str) -> None:
         self.brand = brand
         self.power_rating = power_rating
         self.turned_on: bool = False
 
-        def turn_on(self) -> None:
-            if self.turned_on:
-                print(f"Microwave ({self.brand}) is already turned on.")
-            else:
-                self.turned_on = True
-                print(f"Microwave ({self.brand} is now turned on.)")
-        def turn_off(self) -> None:
-            if self.turned_on:
-                self.turned_on = False
-                print(f"Microwave ({self.brand}) is now turned off.")
-            else:
-                self.turned_on = True
-                print(f"Microwave ({self.brand} is already turned off.)")
-        def run(self,seconds: int) -> None:
-            if self.turned_on:
-                print(f"Running ({self.brand}) for {seconds} seconds")
-            else:
-                print(f"Turn on your microwave first")
+    def turn_on(self) -> None:
+        if self.turned_on:
+            print(f"Microwave ({self.brand}) is already turned on.")
+        else:
+            self.turned_on = True
+            print(f"Microwave ({self.brand}) is now turned on.")
 
-smeg: Microwave = Microwave(brand='Smeg',power_rating= 'B')
+    def turn_off(self) -> None:
+        if self.turned_on:
+            self.turned_on = False
+            print(f"Microwave ({self.brand}) is now turned off.")
+        else:
+            print(f"Microwave ({self.brand}) is already turned off.")
+
+    def run(self, seconds: int) -> None:
+        if self.turned_on:
+            print(f"Running ({self.brand}) for {seconds} seconds")
+        else:
+            print("Turn on your microwave first")
+
+
+smeg: Microwave = Microwave(brand='Smeg', power_rating='B')
+
+smeg.turn_on()
+smeg.run(30)
+smeg.turn_off()
+smeg.run(10)
+
+
+bosch: Microwave = Microwave(brand='Bosch', power_rating='C')
+
+bosch.turn_on()
+bosch.run(30)
+bosch.turn_off()
+bosch.run(10)
