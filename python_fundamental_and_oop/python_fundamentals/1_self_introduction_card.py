93c6a3d889b0905af75db3e88887a8ddd071e772
UlfathAraIslam <mouliajsahid@gmail.com>
2026-08-10 18:05:23 +0900

    add self introduction card program

---
+++ python_fundamental_and_oop/python_fundamentals/1_self_introduction_card.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ").title()
+age = int(input("Enter your age: "))
+city = input("Enter your city: ").title()
+print(f"My name is {name}, I am {age} yours old, and I live in {city}")
