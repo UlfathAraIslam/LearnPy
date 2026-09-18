e14bc785b0e82301fb7be2d8fc62fe3d7bcb7e0c
UlfathAraIslam <mouliajsahid@gmail.com>
2026-09-18 23:08:23 +0900

    create grade book class

--- oop/student_grade_manager.py
+++ oop/student_grade_manager.py
@@ -6,4 +6,40 @@ class Student:
         self.email = email
     def __str__(self):
         return(f"ID: {self.student_id} | Name: {self.name} | Grade: {self.grade} | Email: {self.email}")
-    
+class GradeBook:
+    def __init__(self):
+        self.students = []
+        self.next_id = 1
+    # add student
+    def add_student(self,name,grade,email=None):
+        student = Student(self.next_id, name,grade,email)
+        self.students.append(student)
+        self.next_id += 1
+        print("Student added successfully.")
+    # view students
+    def view_students(self):
+        for student in self.students:
+            print(student)
+    # search student
+    def search_student(self,student_id):
+        for student in self.students:
+            if student.student_id == student_id:
+                print(student)
+                return
+        print("Student not found.")
+    # update grade
+    def update_grade(self,student_id,new_grade):
+        for student in self.students:
+            if student.student_id == student_id:
+                student.grade = new_grade
+                print("Grade updated successfully.")
+                return
+        print("Student not found.")
+    # delete student
+    def delete_student(self,student_id):
+        for student in self.students:
+            if student.student_id == student_id:
+                self.students.remove(student)
+                print("Student removed successfully.")
+                return
+        print("Student not found.")
