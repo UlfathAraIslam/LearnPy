a273393b2da6dd2a1719e4f4ccf32c121aa36055
UlfathAraIslam <mouliajsahid@gmail.com>
2026-09-19 22:07:09 +0900

    create register class to checkout the product

--- oop/convenience_store_register.py
+++ oop/convenience_store_register.py
@@ -6,3 +6,10 @@ class Product:
         print(f"Scanned: {self.name} - {self.price}")
         return self.price
 
+class Register:
+    def __init__(self):
+        self.total = 0
+    def checkout(self,products):
+        for p in products:
+            self.total += p.scan()
+        print(f"Total: ¥{self.total}")
